82bcc19cb3fc640d2c2aef17db9a46aa55754af1
RafalKalita <rafalkalita@gmail.com>
2013-08-15 15:30:58 +0100

    Implementation for test for number two.

--- src/main/java/com/rafalkalita/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/rafalkalita/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,33 @@ package com.rafalkalita.fizzbuzz;
 
 public class FizzBuzz {
 
-	public String generate(int i) {
-		
-		return "1";
+	private String[] values;
+
+	public String generate(int number) {
+
+		values = new String[number];
+
+		for (int i = 1; i <= number; i++) {
+			values[i - 1] = i + "";
+		}
+
+		return printResult();
+	}
+
+	private String printResult() {
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < values.length; i++) {
+			
+			result.append(values[i]);
+
+			if (values.length-1 != i) {
+				result.append(" ");
+			}
+		}
+
+		return result.toString();
 	}
 
 }
